fa28a829a11f8b3ebdab50a247123d7d085eaccc
VCo002 <58818538+VCo002@users.noreply.github.com>
2020-01-14 23:36:56 -0500

    Added comments for variables and method

--- card.java
+++ card.java
@@ -12,19 +12,19 @@ public class card {
 	//PROPERTIES
 	int intShape; //Shape number that can range from 1-32
 	BufferedImage image; //Image associated with each shape
-	boolean blnFlipped = false;
-	int intN;
-	boolean blnPair = false;
-	boolean blnRepeat = false;
+	boolean blnFlipped = false; //Indicates if card is face up or down
+	int intN; //Holds the number associated with each shape
+	boolean blnPair = false; //Indicates if a pair has been found
+	boolean blnRepeat = false; //Indicates if a card faced up has been selected again
 
 	//METHODS
 	 public void flip() {
-		if (this.blnFlipped == true && this.blnPair == true) {
+		if (this.blnFlipped == true && this.blnPair == true) { //keeps card flipped over if user selects a card that's faced up
 			this.blnFlipped = true;
 			blnRepeat = true;
-		}else if (this.blnFlipped == true) {
+		}else if (this.blnFlipped == true) { //flip face up card to be face down
 			this.blnFlipped = false;
-		} else if (this.blnFlipped == false) {
+		} else if (this.blnFlipped == false) { //flip face down card to be face up
 			this.blnFlipped = true;
 		}
 	}
